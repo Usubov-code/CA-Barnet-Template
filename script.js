c546f6eb6cbcd23db315749a1af3eba1b2d002da
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-12 18:05:38 +0400

    Template Web Site

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            items:4,
+            margin:15,
+            loop:true,
+        }
+    );
+  });
+
+
+  $(document).ready(function()
+  {
+$(".custom-tem").on("mouseover",function(){
+
+$("i").css(" ")
+
+
+})
+
+  });
